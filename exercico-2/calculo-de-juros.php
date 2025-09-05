3b8c5f1d8e0965cef254efde7990d8c595cec405
Fabio-h-silva <fabioempresamsp@gmail.com>
2025-09-05 10:25:56 -0300

    exercicio 2 - resolução

--- exercico-2/calculo-de-juros.php
+++ exercico-2/calculo-de-juros.php
@@ -3,13 +3,19 @@
 $capital = $_POST['capital'];
 $taxa = $_POST['taxa'];
 $anos = $_POST['anos'];
-$i = 0;
+$i = 1;
+
+echo "<h1>Resultado de Simulação</h1><br><br>";
+echo "Capital Inicial: $capital <br><br>";
+echo "Taxa de Juros: $taxa <br><br>";
+echo "Duração: $anos <br><br>";
+
 
 $taxa = $taxa/100;
 
 while ($i <= $anos) {
-    $montante = $capital * (1 + $taxa);
+    $montante = $capital * (1 + $taxa)**$i;
+    echo "O valor acumulado no ano $i é de $montante reais <br>";
     $i++;
-    echo "O valor acumulado no $i é de $montante reais";
 }
-
+ echo "<br><a href='index.html'>Voltar</a>";
